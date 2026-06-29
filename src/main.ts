058c3db554fa5631cc482d0c4004b449b5142fff
maoyu <2874553847@qq.com>
2026-06-29 18:00:17 +0800

    feat: 正常学习

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,34 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  // 1. 实例化阶段：通过根模块创建一个 Nest 应用实例
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1');
+
+  // 2. 监听阶段：启动 HTTP 服务器，监听端口
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import {
+//   FastifyAdapter,
+//   NestFastifyApplication,
+// } from '@nestjs/platform-fastify';
+
+// async function bootstrap() {
+//   // 1. 实例化阶段：通过根模块创建一个 Nest 应用实例
+//   const app = await NestFactory.create<NestFastifyApplication>(
+//     AppModule,
+//     new FastifyAdapter(),
+//   );
+
+//   // 2. 监听阶段：启动 HTTP 服务器，监听端口
+//   await app.listen({
+//     port: Number(process.env.PORT) || 3000,
+//     host: '0.0.0.0',
+//   });
+// }
+// void bootstrap();
